c3d3bcf53d4c1e9b0a18f64f3de57d6fc291a78a
vijayshagara <108860027+vijayshagara@users.noreply.github.com>
2022-07-28 00:16:15 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,31 @@
+FOR LOOP
+var obj={
+    "name":"vijay",
+    "age":"25",
+    "DOB":"06aug1997",
+    "location":{
+        "address":"no.5,old kamaraj nagar",
+        "area":"bahour",
+        "state":"puducherry",
+    },
+    }
+    for(var i=0;i<=obj.length;i++){
+    console.log(obj[i]);
+    };
+
+
+    FOR IN LOOP
+
+    var obj={
+        "name":"vijay",
+        "age":"25",
+        "DOB":"06aug1997",
+        "location":{
+            "address":"no.5,old kamaraj nagar",
+            "area":"bahour",
+            "state":"puducherry",
+        },
+        }
+        for(var i in obj){
+            console.log(obj[i]);
+        };
---
+++ json resume.js
@@ -0,0 +1,29 @@
+var resume = {
+    "name": "vijay",
+    "DOB":"06aug1997",
+    "age":"25",
+    "email":"vijayshagara1221@gmail.com",
+    "phone":"8072340596",
+   "location":{
+     "address":"no.5,old kamaraj nagar,bahour ",
+    "state":"puducherry",
+    "PIN code":"607402",
+    "country":"india",
+    },
+    "work":{
+    "name":"tvs pvt ltd",
+    "location":"pondicherry",
+    "position":"mechanical maintenance",
+    "experience":"3 years"
+   },
+   "education":{
+    "institution":"sri ganesh college of engineering and technology,puducherry",
+    "department":"b.tech,mechanical",
+    "CGPA":"6.98",
+    "passed out":"2018",
+   },
+   "languages":{
+    "language":"tamil and english",
+    "fluency":"good",
+   },
+};
